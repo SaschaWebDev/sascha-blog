6c6e32f8fd29607ef87233531cc842f117ccf9ea
Sascha Majewsky <saschamaj@protonmail.com>
2021-11-06 06:26:08 +0100

    Added getrecentposts query and component structure

--- components/PostWidget.jsx
+++ components/PostWidget.jsx
@@ -1,7 +1,23 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import moment from "moment";
+import Link from "next/link";
 
-const PostWidget = () => {
-  return <div></div>;
+import { getRecentPosts, getSimilarPosts } from "../services";
+
+// This component either shows recent posts on the home page or shows related posts for a specific post
+
+const PostWidget = ({ categories, slug }) => {
+  const [highLightedPosts, setHighLightedPosts] = useState([]);
+
+  useEffect(
+    () =>
+      slug
+        ? getSimilarPosts(category).then((result) => setRelatedPosts(result))
+        : getRecentPosts(category).then((result) => setRelatedPosts(result)),
+    []
+  );
+
+  return <div>PostWidget</div>;
 };
 
 export default PostWidget;
