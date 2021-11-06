bc0ef6b80fd5f7fcc6c015ccba4f07c235a9f6fe
Sascha Majewsky <saschamaj@protonmail.com>
2021-11-06 06:50:46 +0100

    Added getsimilarpostquery and styling

--- components/PostWidget.jsx
+++ components/PostWidget.jsx
@@ -12,12 +12,43 @@ const PostWidget = ({ categories, slug }) => {
   useEffect(
     () =>
       slug
-        ? getSimilarPosts(category).then((result) => setRelatedPosts(result))
-        : getRecentPosts(category).then((result) => setRelatedPosts(result)),
-    []
+        ? getSimilarPosts(categories).then((result) =>
+            setHighLightedPosts(result)
+          )
+        : getRecentPosts(categories).then((result) =>
+            setHighLightedPosts(result)
+          ),
+    [slug]
   );
 
-  return <div>PostWidget</div>;
+  return (
+    <div className="p-8 mb-8 bg-white rounded-lg shadow-lg">
+      <h3 className="pb-4 mb-8 text-xl font-semibold border-b">
+        {slug ? "Related Posts" : "Recent Posts"}
+      </h3>
+      {highLightedPosts.map((post, index) => (
+        <div key={index} className="flex items-center w-full mb-4">
+          <div className="flex-none w-16">
+            <img
+              src={post.featuredImage.url}
+              height="60px"
+              width="60px"
+              alt={post.title}
+              className="align-middle rounded-md"
+            />
+          </div>
+          <div className="flex-grow ml-4">
+            <p className="text-gray-500 font-xs">
+              {moment(post.createdAt).format("MMM DD, YYYY")}
+            </p>
+            <Link href={`/post/${post.slug}`} key={index}>
+              {post.title}
+            </Link>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default PostWidget;
--- services/index.js
+++ services/index.js
@@ -59,3 +59,30 @@ export const getRecentPosts = async () => {
 
   return result.posts;
 };
+
+// This query will fetch posts that does not contain the same slug thus exclude the current post but contain some of the same categories.
+
+export const getRSimilarPosts = async () => {
+  const query = gql`
+    query GetPostDetails($slug: String!, $categories: [String!]) {
+      posts(
+        where: {
+          slug_not: $slug
+          AND: { categories_some: { slug_in: $categories } }
+        }
+        last: 3
+      ) {
+        title
+        featuredImage {
+          url
+        }
+        createdAt
+        slug
+      }
+    }
+  `;
+
+  const result = await request(graphqlAPI, query);
+
+  return result.posts;
+};
