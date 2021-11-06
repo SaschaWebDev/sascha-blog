50692c28acc70c8ecf85e102cf2ca551b7606f3e
Sascha Majewsky <saschamaj@protonmail.com>
2021-11-06 08:24:25 +0100

    Created post file structure

--- pages/post/[slug].js
+++ pages/post/[slug].js
@@ -12,7 +12,24 @@ import {
 } from "../../components";
 
 const PostPage = () => {
-  return <div>PostPage</div>;
+  return (
+    <div className="container px-10 mx-auto mb-8">
+      <div className="grid grid-cols-1 gap-12 lg:grid-cols-12">
+        <div className="col-span-1 lg:col-span-8">
+          <PostDetail />
+          <Author />
+          <CommentsForm />
+          <Comments />
+        </div>
+        <div className="col-span-1 lg:col-span-4">
+          <div className="relative lg:sticky top-8">
+            <PostWidget />
+            <Categories />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default PostPage;
