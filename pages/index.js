be707204e70213cb4cbcb853eee5c203c1df0ae3
Sascha Majewsky <saschamaj@protonmail.com>
2021-11-06 01:58:30 +0100

    Example grid structure

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,26 @@
 import Head from "next/head";
 
+const posts = [
+  { title: "My first Post", excerpt: "Hello World!" },
+  { title: "My second Post", excerpt: "Hello World2!" },
+  { title: "My third Post", excerpt: "Hello World3!" },
+];
+
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+    <div className="container px-10 mx-auto mb-8 bg-gray-300">
       <Head>
-        <title>Home</title>
+        <title>SaschaWebDev - Blog</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <div className="grid grid-cols-1 gap-12 lg:grid-cols-12">
+        {posts.map((post, index) => (
+          <div key={index}>
+            {post.title}
+            {post.excerpt}
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
